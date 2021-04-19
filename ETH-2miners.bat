570e53ed2979c3e0ffcfdef052510205e5980d7c
hangryusprime <7278023+hangryusprime@users.noreply.github.com>
2021-04-19 16:47:58 -0400

    more stable core clock, fixed relative log path

--- ETH-2miners.bat
+++ ETH-2miners.bat
@@ -1,8 +1,6 @@
 :START
-start "" "E:\OOSU10\OOSU10.exe"
-::start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile2
-t-rex.exe -c config.json --log-path E:\trex\logs\trex.log
-::taskkill /F /IM MSIAfterburner.exe /t
+start OOSU10.exe
+t-rex.exe -c config.json --log-path "%~dp0\logs\trex.log"
 taskkill /F /IM OOSU10.exe /t
 timeout 30
 goto START
